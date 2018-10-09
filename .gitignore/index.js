c5810851024e09d3dabd438fba6e1fe08a0a1832
Loulougaming09 <40712776+Loulougaming09@users.noreply.github.com>
2018-10-09 20:20:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,11 +12,6 @@ var servers = {};
 
 client.login(process.env.TOKEN);
 
-bot.on("guildMemberAdd", member =>{
-  let role = member.addRole("name", "Abonné Ready");
-  member.addRole(role)
-})
-
 function play(connection, message) {
   
   var server = servers[message.guild.id];
